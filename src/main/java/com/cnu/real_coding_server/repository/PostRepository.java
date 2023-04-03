3708370079c00f5b8652d0268dc916f2a56bc9d8
Yujin-nKim <yujinkimvv@gmail.com>
2023-04-03 18:56:03 +0900

    CRUD 기능 구현

--- src/main/java/com/cnu/real_coding_server/repository/PostRepository.java
+++ src/main/java/com/cnu/real_coding_server/repository/PostRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    // JPA repository를 상속받고 저장할 객체가 무엇인지, id값이 무엇인지만 해 놓으면
+    // 아래는 아래 메소드를 작성하지 않아도 제공해줌
+
 //    Post save(Post post);
 //    List<Post> findAll();
 //    Optional<Post> findById(Integer postId);
--- src/main/java/com/cnu/real_coding_server/service/PostService.java
+++ src/main/java/com/cnu/real_coding_server/service/PostService.java
@@ -1,7 +1,43 @@
 package com.cnu.real_coding_server.service;
 
+import com.cnu.real_coding_server.entity.Post;
+import com.cnu.real_coding_server.model.request.PostRequest;
+import com.cnu.real_coding_server.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public Post createPost(PostRequest postRequest) {
+        //model이여서 저장되는 값은 아니고 entity를 저장할꺼임
+        return postRepository.save(postRequest.toEntity());
+    }
+    public List<Post> getPosts() {
+        return postRepository.findAll();
+    }
+    public Optional<Post> getPost(Integer postId) {
+        return postRepository.findById(postId);
+    }
+    public Optional<Post> updatePost(Integer postId, PostRequest postRequest) {
+        return postRepository.findById(postId)
+                .map(post -> {
+                    post.setTitle(postRequest.getTitle());
+                    post.setContents(postRequest.getContents());
+                    post.setTag(postRequest.getTag());
+                    return postRepository.save(post);
+                });
+    }
+    public void deletePost(Integer postId) {
+        postRepository.findById(postId)
+//                        .ifPresent(post -> postRepository.delete(post));
+                .ifPresent(postRepository::delete);
+
+    }
 }
